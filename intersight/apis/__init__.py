43de1e705d21c30f20b1befbd786266a56e1d301
Vikrant Balyan <vvb@cisco.com>
2021-08-24 05:34:15 +0530

    refreshing sdk for intersight version 4430

---
+++ intersight/apis/__init__.py
@@ -0,0 +1,108 @@
+
+# flake8: noqa
+
+# Import all APIs into this package.
+# If you have many APIs here with many many models used in each API this may
+# raise a `RecursionError`.
+# In order to avoid this, import only the API that you directly need like:
+#
+#   from .api.aaa_api import AaaApi
+#
+# or import this package, but before doing it, use:
+#
+#   import sys
+#   sys.setrecursionlimit(n)
+
+# Import APIs into API package:
+from intersight.api.aaa_api import AaaApi
+from intersight.api.access_api import AccessApi
+from intersight.api.adapter_api import AdapterApi
+from intersight.api.appliance_api import ApplianceApi
+from intersight.api.asset_api import AssetApi
+from intersight.api.bios_api import BiosApi
+from intersight.api.boot_api import BootApi
+from intersight.api.bulk_api import BulkApi
+from intersight.api.capability_api import CapabilityApi
+from intersight.api.certificatemanagement_api import CertificatemanagementApi
+from intersight.api.chassis_api import ChassisApi
+from intersight.api.cloud_api import CloudApi
+from intersight.api.comm_api import CommApi
+from intersight.api.compute_api import ComputeApi
+from intersight.api.cond_api import CondApi
+from intersight.api.config_api import ConfigApi
+from intersight.api.connectorpack_api import ConnectorpackApi
+from intersight.api.crd_api import CrdApi
+from intersight.api.deviceconnector_api import DeviceconnectorApi
+from intersight.api.equipment_api import EquipmentApi
+from intersight.api.ether_api import EtherApi
+from intersight.api.externalsite_api import ExternalsiteApi
+from intersight.api.fabric_api import FabricApi
+from intersight.api.fault_api import FaultApi
+from intersight.api.fc_api import FcApi
+from intersight.api.fcpool_api import FcpoolApi
+from intersight.api.feedback_api import FeedbackApi
+from intersight.api.firmware_api import FirmwareApi
+from intersight.api.forecast_api import ForecastApi
+from intersight.api.graphics_api import GraphicsApi
+from intersight.api.hcl_api import HclApi
+from intersight.api.hyperflex_api import HyperflexApi
+from intersight.api.iaas_api import IaasApi
+from intersight.api.iam_api import IamApi
+from intersight.api.inventory_api import InventoryApi
+from intersight.api.ipmioverlan_api import IpmioverlanApi
+from intersight.api.ippool_api import IppoolApi
+from intersight.api.iqnpool_api import IqnpoolApi
+from intersight.api.iwotenant_api import IwotenantApi
+from intersight.api.kubernetes_api import KubernetesApi
+from intersight.api.kvm_api import KvmApi
+from intersight.api.license_api import LicenseApi
+from intersight.api.ls_api import LsApi
+from intersight.api.macpool_api import MacpoolApi
+from intersight.api.management_api import ManagementApi
+from intersight.api.memory_api import MemoryApi
+from intersight.api.meta_api import MetaApi
+from intersight.api.network_api import NetworkApi
+from intersight.api.networkconfig_api import NetworkconfigApi
+from intersight.api.niaapi_api import NiaapiApi
+from intersight.api.niatelemetry_api import NiatelemetryApi
+from intersight.api.notification_api import NotificationApi
+from intersight.api.ntp_api import NtpApi
+from intersight.api.oprs_api import OprsApi
+from intersight.api.organization_api import OrganizationApi
+from intersight.api.os_api import OsApi
+from intersight.api.pci_api import PciApi
+from intersight.api.port_api import PortApi
+from intersight.api.power_api import PowerApi
+from intersight.api.processor_api import ProcessorApi
+from intersight.api.recommendation_api import RecommendationApi
+from intersight.api.recovery_api import RecoveryApi
+from intersight.api.resource_api import ResourceApi
+from intersight.api.rproxy_api import RproxyApi
+from intersight.api.sdcard_api import SdcardApi
+from intersight.api.sdwan_api import SdwanApi
+from intersight.api.search_api import SearchApi
+from intersight.api.security_api import SecurityApi
+from intersight.api.server_api import ServerApi
+from intersight.api.smtp_api import SmtpApi
+from intersight.api.snmp_api import SnmpApi
+from intersight.api.software_api import SoftwareApi
+from intersight.api.softwarerepository_api import SoftwarerepositoryApi
+from intersight.api.sol_api import SolApi
+from intersight.api.ssh_api import SshApi
+from intersight.api.storage_api import StorageApi
+from intersight.api.syslog_api import SyslogApi
+from intersight.api.tam_api import TamApi
+from intersight.api.task_api import TaskApi
+from intersight.api.techsupportmanagement_api import TechsupportmanagementApi
+from intersight.api.telemetry_api import TelemetryApi
+from intersight.api.terminal_api import TerminalApi
+from intersight.api.thermal_api import ThermalApi
+from intersight.api.top_api import TopApi
+from intersight.api.ucsd_api import UcsdApi
+from intersight.api.uuidpool_api import UuidpoolApi
+from intersight.api.virtualization_api import VirtualizationApi
+from intersight.api.vmedia_api import VmediaApi
+from intersight.api.vmrc_api import VmrcApi
+from intersight.api.vnic_api import VnicApi
+from intersight.api.vrf_api import VrfApi
+from intersight.api.workflow_api import WorkflowApi
---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+NAME = "intersight"
+VERSION = "1.0.9.4430"
+REQUIRES = [
+  "urllib3 >= 1.25.3",
+  "python-dateutil",
+  "pem>=19.3.0",
+  "pycryptodome>=3.9.0",
+]
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description="The Python SDK for Cisco Intersight",
+    author="Intersight API support",
+    author_email="intersight@cisco.com",
+    url="https://intersight.com",
+    keywords=["Cisco Intersight"],
+    python_requires=">=3.6",
+    install_requires=REQUIRES,
+    packages=find_packages(exclude=["test", "tests"]),
+    include_package_data=True,
+    license="MIT",
+    long_description_content_type="text/markdown",
+    long_description=open('README.md').read(),
+)
+
