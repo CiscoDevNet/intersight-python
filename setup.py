88d5a3b5d2dcb21f231ac47780e84179d05dac09
Vikrant Balyan <vvb@users.noreply.github.com>
2025-10-31 15:34:41 +0530

    refreshing sdk for intersight version 20251014120035563 (#89)

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 NAME = "intersight"
-VERSION = "1.0.11.2025092610"
+VERSION = "1.0.11.2025101412"
 REQUIRES = [
   "urllib3 >= 1.25.3",
   "python-dateutil",
